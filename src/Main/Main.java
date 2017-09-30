dfa852482ddf105906d04b3ad4d2b2c9da55641e
Lakshan Thilakarathne <lakshanthilakarathne7@gmail.com>
2017-09-30 19:56:11 +0530

    remove unused library

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -15,7 +15,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
