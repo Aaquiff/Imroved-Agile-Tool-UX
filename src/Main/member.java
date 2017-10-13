0dc968a23c4baa73f3469db52756cd5e221fcc0b
Apisajan <apisajan@ymail.com>
2017-10-13 23:23:48 +0530

    refinemnt in member interface

---
+++ src/Main/member.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author Apisajan
+ */
+public class member {
+    public Button addBtn;
+    public Button updateBtn;
+    public Button deleteBtn;
+    
+    public void initialise(){
+         
+    }
+    public void add (){
+        
+    }
+    public void update(){
+        
+    }
+    public void delete(){
+        
+    }
+}
