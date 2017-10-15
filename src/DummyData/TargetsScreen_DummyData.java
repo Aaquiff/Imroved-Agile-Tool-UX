ee433871e7f3e1009340355980f191dd791df5f0
Lakshan Thilakarathne <lakshanthilakarathne7@gmail.com>
2017-10-15 15:56:51 +0530

    Modified Dumydata for Targets

--- src/DummyData/TargetsScreen_DummyData.java
+++ src/DummyData/TargetsScreen_DummyData.java
@@ -15,38 +15,46 @@ import java.util.Date;
  * @author lathlk
  */
 public class TargetsScreen_DummyData {
-    public ArrayList<Project> projects;
-    public ArrayList<Target> targets;
+    public ArrayList<Project> projects = new ArrayList<Project>();
+    
+    
     public TargetsScreen_DummyData(){
-        projects = null;
+        ArrayList<Target> targets1 = new ArrayList<Target>();   
+        targets1.add(new Target(0, "Target 1", new Date(), new Date(), false));
+        targets1.add(new Target(1, "Target 2", new Date(), new Date(), false));
+        targets1.add(new Target(2, "Target 3", new Date(), new Date(), true));
+        targets1.add(new Target(3, "Target 4", new Date(), new Date(), false));
+        targets1.add(new Target(4, "Target 5", new Date(), new Date(), true));
+        projects.add(new Project("Project 1", "Mfin Mobile Apllication iOS", targets1));
         
-        targets = null;        
-        targets.add(new Target(0, "Target 1", new Date(), new Date(2017, 12, 12), false));
-        targets.add(new Target(1, "Target 2", new Date(), new Date(2017, 9, 12), false));
-        targets.add(new Target(2, "Target 3", new Date(), new Date(2017, 12, 2), true));
-        targets.add(new Target(3, "Target 4", new Date(), new Date(2018, 01, 6), false));
-        targets.add(new Target(4, "Target 5", new Date(), new Date(2018, 02, 02), true));
-        projects.add(new Project("Project 1", "Mfin Mobile Apllication", targets));
+        ArrayList<Target> targets2 = new ArrayList<Target>();   
+        targets2.add(new Target(0, "Target 1", new Date(), new Date(), false));
+        targets2.add(new Target(1, "Target 2", new Date(), new Date(), false));
+        targets2.add(new Target(2, "Target 3", new Date(), new Date(), true));
+        targets2.add(new Target(3, "Target 4", new Date(), new Date(), false));
+        targets2.add(new Target(4, "Target 5", new Date(), new Date(), true));
+        projects.add(new Project("Project 2", "Mfin Web Apllication", targets1));
         
-        targets = null;        
-        targets.add(new Target(1, "Target 1", new Date(), new Date(2017, 12, 12), false));
-        targets.add(new Target(2, "Target 2", new Date(), new Date(2017, 9, 12), false));
-        targets.add(new Target(3, "Target 3", new Date(), new Date(2017, 12, 2), true));
-        targets.add(new Target(4, "Target 4", new Date(), new Date(2018, 01, 6), false));
-        targets.add(new Target(5, "Target 5", new Date(), new Date(2018, 02, 02), true));
-        projects.add(new Project("Project 2", "Mfin Mobile Apllication", targets));
+        ArrayList<Target> targets3 = new ArrayList<Target>();   
+        targets3.add(new Target(0, "Target 1", new Date(), new Date(), false));
+        targets3.add(new Target(1, "Target 2", new Date(), new Date(), false));
+        targets3.add(new Target(2, "Target 3", new Date(), new Date(), true));
+        targets3.add(new Target(3, "Target 4", new Date(), new Date(), false));
+        targets3.add(new Target(4, "Target 5", new Date(), new Date(), true));
+        projects.add(new Project("Project 3", "Mfin Backend Server Application", targets3));
         
-        targets = null;        
-        targets.add(new Target(1, "Target 1", new Date(), new Date(2017, 12, 12), false));
-        targets.add(new Target(2, "Target 2", new Date(), new Date(2017, 9, 12), false));
-        targets.add(new Target(3, "Target 3", new Date(), new Date(2017, 12, 2), true));
-        targets.add(new Target(4, "Target 4", new Date(), new Date(2018, 01, 6), false));
-        targets.add(new Target(5, "Target 5", new Date(), new Date(2018, 02, 02), true));
-        projects.add(new Project("Project 3", "Mfin Mobile Apllication", targets));
+        ArrayList<Target> targets4 = new ArrayList<Target>();   
+        targets4.add(new Target(0, "Target 1", new Date(), new Date(), false));
+        targets4.add(new Target(1, "Target 2", new Date(), new Date(), false));
+        targets4.add(new Target(2, "Target 3", new Date(), new Date(), true));
+        targets4.add(new Target(3, "Target 4", new Date(), new Date(), false));
+        targets4.add(new Target(4, "Target 5", new Date(), new Date(), true));
+        projects.add(new Project("Project 4", "Mfin Mobile Apllication Android", targets4));
     }
     
     public ArrayList<Project> getDummyProjects()
     {
         return projects;
     }
+    
 }
--- src/Main/MainPageController.java
+++ src/Main/MainPageController.java
@@ -6,6 +6,7 @@
 package Main;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -17,7 +18,10 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
-
+import DummyData.TargetsScreen_DummyData;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import models.Project;
 /**
  * FXML Controller class
  *
@@ -68,18 +72,31 @@ public class MainPageController implements Initializable {
     @FXML
     private TableColumn<?, ?> SearchColSprint;
     @FXML
-    private ListView<?> Targets_ProjectsList;
+    private ListView<String> Targets_ProjectsList;
     @FXML
     private Text Targets_TargetLabel;
-
+    ObservableList<String> projectsList = FXCollections.observableArrayList();
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // Targets Screen
-        
+        getTargetScreenData();
         // Targets Screen
     }    
+
+    private void getTargetScreenData() {
+        TargetsScreen_DummyData dummy = new TargetsScreen_DummyData();        
+        ArrayList<Project> projects = dummy.getDummyProjects();        
+        projectsList.clear();
+        ArrayList<String> projNames = new ArrayList<String>();
+        for(Project p : projects)
+        {
+            projNames.add(p.name);
+        }
+        projectsList.addAll(projNames);
+        Targets_ProjectsList.setItems(projectsList);
+    }
     
 }
