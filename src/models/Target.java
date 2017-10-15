8566b0af532ebf2420eb31af1b73e2504a375fd0
Lakshan Thilakarathne <lakshanthilakarathne7@gmail.com>
2017-10-15 13:53:03 +0530

    Addded Target Model

---
+++ src/models/Target.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.util.Date;
+
+/**
+ *
+ * @author lathlk
+ */
+public class Target {
+    public int targetId;
+    public String targetDescription;
+    public Date created;
+    public Date dueDate;
+    public boolean status;    
+}
