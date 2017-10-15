ad3bf09c62fe72b398fce33af59a4a794c62c0b9
Dimansha Wijebandara <dimansha.wijebandara@gmail.com>
2017-10-15 17:00:44 +0530

    list view projects

--- src/Main/ProjectsViewController.java
+++ src/Main/ProjectsViewController.java
@@ -9,16 +9,21 @@ import java.io.IOException;
 import static java.lang.Compiler.disable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.DialogPane;
+import javafx.scene.control.ListView;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javax.swing.JOptionPane;
+import models.Project;
 
 
 
@@ -43,5 +48,14 @@ public class ProjectsViewController {
         dialog.setScene(dialogScene);
         dialog.show();
 
+    }
+    @FXML
+    private ListView projectListView;
+    public void AddNewProject(Project p){
+        ListView<String> projectListView = new ListView<String>();
+        ObservableList<String> items =FXCollections.observableArrayList (
+            p.name);
+        projectListView.setItems(items);
+                
     }
 }
